69c9e97ed8938dfce726a18d362e971ca78ab9da
darskp <darshanpatgar100@gmail.com>
2024-01-05 16:43:48 +0530

    Student class

---
+++ src/db/DBconnection.java
@@ -0,0 +1,24 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBconnection {
+
+  static Connection con;
+
+  public static Connection createConnection() {
+    try {
+      Class.forName("com.mysql.cj.jdbc.Driver");
+      con=DriverManager.getConnection(
+        "jdbc:mysql://localhost:3306/studentdb?autoReconnect=true&useSSL=false",
+        "root",
+        "Admin@123#"
+      );
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return con;
+  }
+}
---
+++ src/model/Student.java
@@ -0,0 +1,56 @@
+package model;
+
+public class Student {
+    private int rollNum;
+    private String name;
+    private String clgname;
+    private String city;
+    private double percentage;
+    
+    @Override
+    public String toString() {
+        return "Student [rollNum=" + rollNum + ", name=" + name + ", clgname=" + clgname + ", city=" + city
+                + ", percentage=" + percentage + "]";
+    }
+
+
+    public Student(int rollNum, String name, String clgname, String city, double percentage) {
+        this.rollNum = rollNum;
+        this.name = name;
+        this.clgname = clgname;
+        this.city = city;
+        this.percentage = percentage;
+    }
+
+    
+    public int getRollNum() {
+        return rollNum;
+    }
+    public void setRollNum(int rollNum) {
+        this.rollNum = rollNum;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getClgname() {
+        return clgname;
+    }
+    public void setClgname(String clgname) {
+        this.clgname = clgname;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public double getPercentage() {
+        return percentage;
+    }
+    public void setPercentage(double percentage) {
+        this.percentage = percentage;
+    }
+}
